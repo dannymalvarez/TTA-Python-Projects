b879bed3b7b4c3749586b7b04332555b445f272b
Daniel Alvarez <dannymalvarez@gmail.com>
2021-12-14 16:07:41 -0500

    adding follow along nice or mean game

---
+++ niceOrMean/niceOrMean.py
@@ -0,0 +1,27 @@
+# Python 3.10.1
+# Author: Daniel M Alvarez
+# First program created by following along
+# with course for The Tech Academy
+# Software Developer Boot Camp.
+
+
+def start():
+    print(get_number())
+
+
+
+
+
+def get_number():
+    number = 12
+    return number
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    start()
