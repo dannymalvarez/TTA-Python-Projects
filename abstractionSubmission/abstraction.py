bb054e087aaa38b188d8b8f21811d7b3c6a8e34f
Daniel Alvarez <dannymalvarez@gmail.com>
2021-12-17 10:44:44 -0500

    finished abstract assignment with revisions
    watched a yt video that helped clear up and make sense of what abstract methods were. very helpful

--- abstractionSubmission/abstraction.py
+++ abstractionSubmission/abstraction.py
@@ -3,33 +3,38 @@
 Create a class that utilizes the concept of abstraction.
 
 
-Your class should contain at least one abstract method and one regular method.  
+Your class should contain at least one abstract
 
-Create a child class that defines the implementation of its parents abstract method.
+method and one regular method.  
 
-Create an object that utilizes both the parent and child methods.
+Create a child class that defines the
+
+implementation of its parents abstract method.
+
+Create an object that utilizes both
+
+the parent and child methods.
  
 
 Add comments throughout your Python explaining your code.
 
 
 '''
-from abc import ABC, abstractmethod
-class car(ABC):
-    def paySlip(self,amount):
-        print('Your purchase amount: ',amount)
-# this function is telling us to pass in an argument.
-# but we won't tell you how or what kind of data that will be
-    @abstractmethod
-    def payment(self,amount):
-        pass
 
-class DebitCardPayment(car):
-# Here we've defined how to implement the payment function 
-# from its parent paySlip class
-    def payment(self,amount):
-        print('Your purchase amount of {} exceeded your $100 limit '.format(amount))
 
-obj = DebitCardPayment()
-obj.paySlip('$400')
-obj.payment('$400')
+from abc import ABC, abstractmethod #importing abstractmethod from the abc module
+class phone(ABC): #creating a parent class as an abstract base class
+    def phoneBill(self,amount): #creating a method with parent class inheritance and a new input
+        print('Your monthly total is: ',amount)#this function prints a message with the input amount
+    @abstractmethod #activating abstractmethod
+    def payment(self,amount): #creating an abstract method with parent inheritance and an input amount
+        pass #method with no body/content
+
+class CreditCardPayment(phone): #creating a new parents class that inherits from the previous parent class
+    def payment(self,amount): #defining the method again without pass this time
+        print('Your monthly bill of ' + amount + ' was accepted and processed successfully.')
+
+
+pay = CreditCardPayment() #instantiating the CCP class which runs the now defined payment method
+pay.phoneBill('$50') #instantiating CCP class with phoneBill method
+pay.payment('$50') #instantiating CCP class with payment (abstract) method
