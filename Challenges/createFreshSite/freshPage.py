7f93fbb3fbf784d675891fe9c2088d2bdd422b81
Daniel Alvarez <dannymalvarez@gmail.com>
2021-12-20 16:51:07 -0500

    fixed previous assignment

---
+++ Challenges/createFreshSite/freshPage.py
@@ -0,0 +1,8 @@
+
+import webbrowser as w
+#f = open('newPage.html','x')
+#f = open('newPage.html','a')
+#f.write('<html><body><h1>Stay tuned for our amazing summer sale!</h1></body></html>')
+#f.close()
+
+w.open('/Users/danielalvarez/Documents/GitHub/TTA-Python-Projects/Challenges/createFreshSite/newPage.html',new=2)
