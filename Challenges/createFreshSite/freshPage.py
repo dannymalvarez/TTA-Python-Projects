264087369928c55dcc514d3bc604551825b6575f
Daniel Alvarez <dannymalvarez@gmail.com>
2021-12-20 17:27:55 -0500

    finished new tab html file script

--- Challenges/createFreshSite/freshPage.py
+++ Challenges/createFreshSite/freshPage.py
@@ -1,8 +1,29 @@
 
 import webbrowser as w
-#f = open('newPage.html','x')
-#f = open('newPage.html','a')
-#f.write('<html><body><h1>Stay tuned for our amazing summer sale!</h1></body></html>')
-#f.close()
+import os
 
-w.open('/Users/danielalvarez/Documents/GitHub/TTA-Python-Projects/Challenges/createFreshSite/newPage.html',new=2)
+
+go = 'newPage.html'
+place = '/Users/danielalvarez/Documents/GitHub/TTA-Python-Projects/Challenges/createFreshSite/'
+
+placeCheck = os.listdir(place)
+abPath = os.path.join(place,go)
+final = 'file://' + abPath
+print(final)
+
+if go in placeCheck:
+    f = open('newPage.html','r')
+    f.close()
+    w.get('chrome').open(final)
+    
+else:
+    f = open('newPage.html','w')
+    f.write('<html><body><h1>Stay tuned for our amazing summer sale!</h1></body></html>')
+    f.close()
+    w.get('chrome').open(final)
+
+
+
+
+
+
