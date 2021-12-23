02532ba7c3e83d011dfc168398823509d7f8a414
Daniel Alvarez <dannymalvarez@gmail.com>
2021-12-23 10:11:42 -0500

    finishing assignment for submission

--- Challenges/createFreshSite/freshPage.py
+++ Challenges/createFreshSite/freshPage.py
@@ -1,7 +1,9 @@
 
+import tkinter
+from tkinter import *
 import webbrowser as w
 import os
-
+     
 
 go = 'newPage.html'
 place = '/Users/danielalvarez/Documents/GitHub/TTA-Python-Projects/Challenges/createFreshSite/'
@@ -9,21 +11,54 @@ place = '/Users/danielalvarez/Documents/GitHub/TTA-Python-Projects/Challenges/cr
 placeCheck = os.listdir(place)
 abPath = os.path.join(place,go)
 final = 'file://' + abPath
-print(final)
+
 
 if go in placeCheck:
     f = open('newPage.html','r')
     f.close()
     w.get('chrome').open(final)
-    
+        
 else:
-    f = open('newPage.html','w')
-    f.write('<html><body><h1>Stay tuned for our amazing summer sale!</h1></body></html>')
-    f.close()
-    w.get('chrome').open(final)
+    class ParentWindow(Frame):
+        def __init__ (self,master):
+            Frame.__init__ (self)
+
+            self.master = master
+            self.master.title('What would you like the page to display?')
+            self.master.config(bg='lightgray')
+              
+
+            self.siteInput = StringVar()
+
+            self.lblsiteInput = Label(self.master,text='Type here: ', font=('Helvetica',16),fg='black',bg='lightgray' )
+            self.lblsiteInput.grid(row=0,column=0,padx=(30,0),pady=(30,0))
+
+            self.txtsiteInput = Entry(self.master,text=self.siteInput,font=('Helvetica',16),fg='black',bg='lightblue')
+            self.txtsiteInput.grid(row=0,column=1,padx=(30,0),pady=(30,0))
+
+            self.btnSubmit = Button(self.master,text='Submit', width=6, height=2, command=self.submit)
+            self.btnSubmit.grid(row=2,column=1,padx=(0,0),pady=(30,0), sticky=NE)
+
+            self.btnCancel = Button(self.master,text='Cancel', width=6, height=2, command=self.cancel)
+            self.btnCancel.grid(row=2,column=1,padx=(0,110),pady=(30,0), sticky=NE)
 
 
 
+        def submit(self):
+            info = self.txtsiteInput.get()
+            f = open('newPage.html','w')
+            f.write('<html><body><h1>'+info+'</h1></body></html>')
+            f.close()
+            w.get('chrome').open(final)
+
+        def cancel(self):
+            self.master.destroy()
+
+
+if __name__ == '__main__':
+    root = Tk()
+    App = ParentWindow(root)
+    root.mainloop()
 
 
 
