d32773334876403c64b1499d43379a81f79ec19b
Daniel Alvarez <dannymalvarez@gmail.com>
2021-12-23 19:53:21 -0500

    finished commenting code

--- Challenges/fileTransfer/fileTransfer.py
+++ Challenges/fileTransfer/fileTransfer.py
@@ -6,26 +6,26 @@ import tkinter as tk
 from tkinter import *
 from tkinter import filedialog as fd
 
-class ParentWindow(Frame):
+class ParentWindow(Frame): #gui
     
     def __init__ (self,master):
         Frame.__init__(self)
     
         self.master = master
-        self.master.minsize(600,250)
+        self.master.minsize(600,250)#setting a size for it to fit and look nice
         self.master.maxsize(600,250)
-        self.master.title("Check files")
-        self.master.configure(bg="#F0F0F0")
+        self.master.title("Check files") #title of the gui window
+        self.master.configure(bg="#F0F0F0") #gui color for background
 
         #configuring the grid
         self.master.columnconfigure(0, weight=1)
-        self.master.columnconfigure(1, weight=3)
+        self.master.columnconfigure(1, weight=3) #this was the only way I could get the length of the columnspan columns to work right
   
-        self.varEmpty1 = tk.StringVar()
+        self.varEmpty1 = tk.StringVar() #setting variables to be strings using tkinter
         self.varEmpty2 = tk.StringVar()
         self.varEmpty3 = tk.StringVar()
         
-
+# setting empty variables to tkinter entry methods with positioning, font, font size, and other info
         self.txtEmpty1 = tk.Entry(self.master,text=self.varEmpty1,font=("Helvetica", 25),fg='black',bg='white')
         self.txtEmpty1.grid(row=0,column=1,padx = (0,20),pady=(30,0),columnspan=2,sticky=tk.E+W)
 
@@ -34,7 +34,7 @@ class ParentWindow(Frame):
 
         self.txtEmpty3 = tk.Entry(self.master,text=self.varEmpty3,font=("Helvetica", 25),fg='black',bg='white')
         self.txtEmpty3.grid(row=2,column=1,padx = (0,20),pady=(15,0),sticky=tk.E+W)
-
+# creating button that use commands from below
         self.button1 = tk.Button(self.master, text = "Source Folder", width = 10,height=2,command=self.source)
         b1 = self.button1
         b1.grid(row = 0, column = 0,padx=(20,10),pady=(30,0),sticky=tk.W)
@@ -57,14 +57,14 @@ class ParentWindow(Frame):
         self.varEmpty1.set(sourcePath)
 
 
-#set the destination path to folderB
+#set the destination path to folder of choice
         
     def destination(self):
         destinationPath = fd.askdirectory()
         self.varEmpty2.set(destinationPath)
 
 
-
+# method to check using comparisons accurate to the hour between files to determine which should be moved
     def check(self):
         now = datetime.now()
         currentHour = now.strftime('%I')
@@ -75,9 +75,9 @@ class ParentWindow(Frame):
         source = self.varEmpty1.get()
         files = os.listdir(source)
         destination = self.varEmpty2.get()
-        print(files)
-        print(destination)
-        print(source)
+        print(files)# print files in the source path, this helped me figure out what was wrong which was that there was a .DS_Store file
+        print(destination) #print destination path 
+        print(source) #print source path
 
         for i in files:
             modTime = (time.ctime(os.path.getmtime(source+"/"+i)))
@@ -88,10 +88,10 @@ class ParentWindow(Frame):
             modTimeYear = modTimeObj.strftime("%Y")
 
             if currentYear == modTimeYear and currentMonth == modTimeMonth and currentDay == modTimeDay and currentHour and modTimeHour:
-                self.varEmpty3.set('Files transferred.')
+                self.varEmpty3.set('Files transferred.') #making use of the third entry box by having it display some text based on success or failure of method.
                 shutil.move(source+"/"+i,destination)
             else:
-                self.varEmpty3.set('There were no files to transfer')
+                self.varEmpty3.set('There were no files to transfer') 
 
 
     def close(self):
